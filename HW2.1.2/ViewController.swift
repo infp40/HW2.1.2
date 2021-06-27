5cdbdac4cff173437322c989dc794bea5eda35ca
maxim palekhin <infp40@gmail.com>
2021-06-28 00:13:20 +0300

    Remake all works

--- HW2.1.2/ViewController.swift
+++ HW2.1.2/ViewController.swift
@@ -7,13 +7,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum CurrentLight {
+    case red, yellow, green
+}
 
+class ViewController: UIViewController {
+    @IBOutlet weak var redLight: UIView!
+    @IBOutlet weak var yellowLight: UIView!
+    @IBOutlet weak var greenLight: UIView!
+    
+    @IBOutlet weak var redLightForPeople: UIImageView!
+    @IBOutlet weak var greenLightForPeople: UIImageView!
+    
+    @IBOutlet weak var pushButton: UIButton!
+    
+    private var currentLight = CurrentLight.red
+    private let lightIsOn: CGFloat = 1
+    private let lightIsOff: CGFloat = 0.3
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLight.alpha = lightIsOff
+        yellowLight.alpha = lightIsOff
+        greenLight.alpha = lightIsOff
+        redLightForPeople.alpha = lightIsOff
+        greenLightForPeople.alpha = lightIsOff
     }
-
-
 }
 
